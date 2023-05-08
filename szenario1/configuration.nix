bc472c7e8098cfb534b9451cced0129a91d18a0d
Powerbuilder_1 <philipp.120201@gmail.com>
2023-05-08 10:55:14 +0200

    delete NIX_PATH command

--- szenario1/configuration.nix
+++ szenario1/configuration.nix
@@ -118,7 +118,5 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
 
-  nix.nixPath = [ "nixos-config=/etc/nixos/shared/configuration.nix" ];
-
 }
 
