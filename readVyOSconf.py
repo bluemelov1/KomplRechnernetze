e3267494fe17436b5e10b807b4bcff9ab4f2100b
bluemelov1 <hannes.bluemel@mailbox.tu-dresden.de>
2023-06-29 15:47:57 +0200

    added transformer of initial vyos config and some example

---
+++ readVyOSconf.py
@@ -0,0 +1,37 @@
+import json
+
+#path='szenario1/vyos/bonding/config.json'
+path='szenario2/vyos/config-client.json'
+
+def generate_entry_strings(data, prefix=''):
+    entries = []
+
+    if isinstance(data, dict):
+        for key, value in data.items():
+            entry = f"{prefix}{key}"  # Aktuelle Schlüsselzeichenkette
+
+            if isinstance(value, dict) or isinstance(value, list):
+                sub_entries = generate_entry_strings(value, prefix=f"{entry}#")  # Generiere rekursiv Untereinträge für dict oder list
+                entries.extend(sub_entries)
+            else:
+                entries.append(f"{entry}:{value}")  # Füge die aktuelle Schlüsselzeichenkette mit Wert hinzu
+
+    elif isinstance(data, list):
+        for item in data:
+            entry = f"{prefix}"  # Aktuelle Indexzeichenkette
+            entries.append(f"{entry}:{item}")
+            
+    return entries
+
+# Lese die JSON-Datei ein
+with open(path) as file:
+    json_data = json.load(file)
+
+# Generiere Array von Eintragszeichenketten mit inneren Werten
+entries = generate_entry_strings(json_data)
+
+
+# Gebe das Array aus
+for entry in entries:
+    print(entry)
+print(len(entries))
