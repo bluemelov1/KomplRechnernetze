5ce6acb23f80e5af335049ed10b1ac3b55b3f9d4
Powerbuilder_1 <philipp.120201@gmail.com>
2023-07-08 23:14:38 +0200

    add auto config script for virtual interfaces

---
+++ create_tuntap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+interfaces=("p1" "p2" "c0" "c1")
+
+for interface in "${interfaces[@]}"
+do
+    # Erstellen der Tuntap-Schnittstelle
+    ip tuntap add "$interface" mode tap
+
+    # Aktivieren der Schnittstelle
+    ip link set dev "$interface" up
+done
+
+echo "Tuntap-Schnittstellen wurden erfolgreich erstellt und aktiviert."
+
